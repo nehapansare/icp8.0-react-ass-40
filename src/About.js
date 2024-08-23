aaa1c0f5fb6c775a525ae1c5f5e47d9fc6503040
nehapansare <nehapansare007@gmail.com>
2024-08-23 10:59:06 +0530

    about.js code added

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function About() {
+  return (
+    <div>
+        <Navbar/>
+      <h1 className='page-title'>About Us</h1>
+      
+    </div>
+  )
+}
+
+export default About
