657157594e413e57565450ab1ce9e158f23659a6
nehapansare <nehapansare007@gmail.com>
2024-08-23 10:59:38 +0530

    contact.js code added

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../src/Navbar';
+
+
+function Contact() {
+  return (
+    <div>
+        <Navbar/>
+        <h1 className='page-title'>Contact Us Page</h1>
+        
+      
+    </div>
+  )
+}
+
+export default Contact
