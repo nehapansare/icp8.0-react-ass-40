30ae5ae7db334cc239b7d464af33451bb78234f4
nehapansare <nehapansare007@gmail.com>
2024-08-23 10:58:38 +0530

    home.js code added

--- src/Home.js
+++ src/Home.js
@@ -1,1 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
 
+
+
+function Home() {
+  return (
+    <div>
+        <Navbar/>
+      <h1 className='page-title'>Home Page</h1>
+      
+    </div>
+  )
+}
+
+export default Home
